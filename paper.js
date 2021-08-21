a8d55641902a74cba1ed14a03382c6773cae576f
ilovejavascript369 <85027578+ilovejavascript369@users.noreply.github.com>
2021-08-21 09:51:44 -0500

    Add files via upload

--- paper.js
+++ paper.js
@@ -1,22 +1,31 @@
-class BaseClass{
-    constructor(x, y) {
+class Paper{
+    constructor(x,y){
         var options = {
             restitution: 1
         }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
+        //this.body = Bodies.circle(x, y, width, height,options);
+        this.radius = 70;
+        this.body = Bodies.circle(x, y, this.radius,options);
         this.image = loadImage("paper.png");
-        World.add(world, this.body);
-      }
-      display(){
+        //Matter.Body.setAngle(this.body,angle)
+        World.add(world,this.body)
+    }
+    display(){
+        var pos = this.body.position;
         var angle = this.body.angle;
         push();
-        translate(this.body.position.x, this.body.position.y);
+        translate(pos.x,pos.y);
         rotate(angle);
+        //rectMode(CENTER);
+        fill("white");
+        stroke("white");
+        ellipseMode(RADIUS);
+        ellipse(0,0,this.radius,this.radius)
+        //rect(0,0,this.width,this.height);
         imageMode(CENTER);
         image(this.image, 0, 0, this.width, this.height);
-
+        
         pop();
-      }
+    }
+    
 }
